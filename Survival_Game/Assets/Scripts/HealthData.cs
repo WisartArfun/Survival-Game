51f3689ab2c8abd2c25dc1005fe1cf43dd8694bb
WisartArfun <daro.daro15@gmail.com>
2018-08-03 11:48:52 +0200

    adding already created files

---
+++ Survival_Game/Assets/Scripts/HealthData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthData : MonoBehaviour {
+
+	public float currentHealth;
+	// public bool alive;
+		
+}
---
+++ Survival_Game/Assets/Scripts/Ishift.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Ishift {
+	void Translate (float x, float y);
+}
